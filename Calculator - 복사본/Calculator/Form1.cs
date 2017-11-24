7cdcec04ce3330d6bfb6d6e3446714eaa1555fc9
민규 김 <pb1123love@gmail.com>
2017-11-24 22:08:42 +0900

    2017_11_24 22:07 C# Team Project - Calculator Step4 success

--- Calculator - 복사본/Calculator/Form1.cs
+++ Calculator - 복사본/Calculator/Form1.cs
@@ -307,19 +307,19 @@ namespace Calculator
             }
         }
 
-        private void Negative_Click(object sender, EventArgs e)
+        private void Negative_Click(object sender, EventArgs e) //± 버튼 클릭 처리
         {
-            int temp = int.Parse(view);
-            temp = temp * -1;
-            view = temp.ToString();
+            double temp = double.Parse(view);// 현재 입력된 값을 실수로 옮김
+            temp = temp * -1; //그 후 -1을 곱함 (음수면 양수 양수면 음수)
+            view = temp.ToString(); //다시 문자열화 시켜서 view에 저장
             history = temp.ToString();
             ShowHistory(history);
             ShowText(view);
         }
 
-        private void Cancel_Click(object sender, EventArgs e)
+        private void Cancel_Click(object sender, EventArgs e) //C 버튼 클릭 처리
         {
-            view = null;
+            view = null; //view, history total textbox1 textbox2 모두 클리어
             history = null;
             total = null;
             textBox1.Clear();
@@ -328,23 +328,23 @@ namespace Calculator
             ShowText(view);
         }
 
-        private void ButtonDot_Click(object sender, EventArgs e)
+        private void ButtonDot_Click(object sender, EventArgs e) //.버튼 클릭 처리
         {
-            if (!view.Contains("."))
+            if (!view.Contains(".")) //현재 값에 .이 없다면 
             {
-                history += ".";
+                history += "."; //현재 값 끝에 . 추가
                 view += ".";
                 ShowHistory(history);
                 ShowText(view);
             }
         }
 
-        private void Result_Click(object sender, EventArgs e)
+        private void Result_Click(object sender, EventArgs e) // = 버튼 처리
         {
-            if (view != null)
+            if (view != null) //view가 null이 아니면 즉 입력값이 있다면
             {
-                total = ResultValue(double.Parse(view));
-                if (DivisionError(total) == 0)
+                total = ResultValue(double.Parse(view)); //현재 값 까지 최종값 계산
+                if (DivisionError(total) == 0) //0 나누기 에러 처리
                 {
                     ShowText(total);
                     view = null;
@@ -354,44 +354,42 @@ namespace Calculator
                 }
                 else
                 {
-                    history += "=";
-                    history += total;
+                    history += "="; //기록에 = 추가
+                    history += total; //최종값 추가
                     ShowHistory(history);
 
-                    view = null;
-                    ShowText(total);
+                    view = null; //현재 입력값 초기화
+                    ShowText(total); //최종값 화면에 보이기
 
-                    input.Clear();
-                    historys.Add(history);
-                    historys.Add("---------------------------------------------");
-                    RebindingListBox(listBox1, historys);
-                    history = null;
+                    input.Clear(); //연산식 초기화
+                    historys.Add(history); //현재 연산기록을 리스트 박스에 등록하기위해 List에 추가
+                    historys.Add("---------------------------------------------"); //구분선 추가
+                    RebindingListBox(listBox1, historys); //Listbox 업데이트
+                    history = null; //현재 연산기록 초기화
 
                 }
             }
             else
             {
-                ShowText(total);
+                ShowText(total); //현재 입력값이 없다면 최종값 화면에 보이기
             }
         }
 
-        private void M_plus_Click(object sender, EventArgs e)
+        private void M_plus_Click(object sender, EventArgs e) //M+ 버튼 처리
         {
-            if (memory == null)
+            if (memory == null) //memory 가 null 즉 현재값을 메모리에 등록할 때에
             {
-                if (view != null)
+                if (view != null) //view가 널이 아니면 즉 입력값이 존재 할때
                 {
-                    if (total == null)
+                    if (total == null) //total 이 null일때 즉 계산식이 없고 입력값만 존재할 때
                     {
-                        total = view;
+                        memory = string.Copy(view); //현재 입력값을 memory에 등록
                         view = null;
-                        memory = string.Copy(total);
-                        total = null;
                     }
-                    else
+                    else //total이 존재 할 때 즉 계산식이 존재할때
                     {
-                        total = ResultValue(double.Parse(view));
-                        if (DivisionError(total) == 0)
+                        total = ResultValue(double.Parse(view)); //현재값까지 추가해서 최종 연산값 추출
+                        if (DivisionError(total) == 0) //0 나누기 에러 처리
                         {
                             ShowText(total);
                             view = null;
@@ -401,34 +399,32 @@ namespace Calculator
                         }
                         else
                         {
-                            view = null;
-                            history = null;
-                            memory = string.Copy(total);
-                            total = null;
-                            input.Clear();
+                            view = null; //입력값 초기화
+                            history = null; //기록 초기화
+                            memory = string.Copy(total); //메모리에 최종연산값 등록
+                            total = null; //최종값 초기화
+                            input.Clear(); //연산식 초기화
                         }
                     }
                 }
             }
-            else
+            else //메모리에 값이 존재할 때 즉 기존 메모리값에 현재 값을 더할 때
             {
-                if (view != null)
+                if (view != null) //view가 null이 아닐때 즉 입력값이 존재할때
                 {
-                    if (total == null)
+                    if (total == null) //total이 null일때 즉 연산이 없고 첫번째 입력값일때
                     {
-                        total = view;
-                        view = null;
-                        double tmp = double.Parse(memory);
-                        tmp += double.Parse(total);
-                        memory = tmp.ToString();
-                        total = null;
+                        double tmp = double.Parse(memory); //메모리값을 읽어와서 double로 변경
+                        tmp += double.Parse(view); //현재 입력값을 기존 메모리값에 더함
+                        memory = tmp.ToString(); //더한 값을 memory에 저장
+                        view = null; //현재값 초기화
                     }
-                    else
+                    else //total이 null이 아닐때 즉 계산식이 존재할 때
                     {
-                        total = ResultValue(double.Parse(view));
-                        if (DivisionError(total) == 0)
+                        total = ResultValue(double.Parse(view)); //현재값을 추가해서 최종연산값 추출
+                        if (DivisionError(total) == 0) //0 나누기 에러 처리 (에러 발생시 0 리턴)
                         {
-                            ShowText(total);
+                            ShowText(total); //에러 발생시 모두 초기화
                             view = null;
                             total = null;
                             history = null;
@@ -436,37 +432,37 @@ namespace Calculator
                         }
                         else
                         {
-                            view = null;
-                            history = null;
+                            view = null; //입력값 초기화
+                            history = null; //기록 초기화
 
-                            double tmp = double.Parse(memory);
-                            tmp += double.Parse(total);
-                            memory = tmp.ToString();
-                            total = null;
-                            input.Clear();
+                            double tmp = double.Parse(memory); //메모리값 읽어오기
+                            tmp += double.Parse(total); //최종연산값 기존 메모리값에 더하기
+                            memory = tmp.ToString(); //메모리에 저장
+                            total = null; //최종값 날리기
+                            input.Clear(); //연산식 날리기
                         }
                     }
                 }
             }
         }
 
-        private void MR_Click(object sender, EventArgs e)
+        private void MR_Click(object sender, EventArgs e) //MR 버튼 처리
         {
-            history = null;
-            view = null;
-            history += memory;
+            history = null; //기록 초기화
+            view = null; //입력값 초기화
+            history += memory; //기록 -> 메모리에서 로드한 값으로 변경
 
-            view += memory;
-            ShowHistory(history);
+            view += memory; //입력값 을 메모리에서 로드한 값으로 변경
+            ShowHistory(history); //화면 띄우기
             ShowText(view);
         }
 
-        private void MC_Click(object sender, EventArgs e)
+        private void MC_Click(object sender, EventArgs e) //MC 버튼 처리
         {
-            memory = null;
+            memory = null; //memory값 초기화
         }
 
-        private void M_minus_Click(object sender, EventArgs e)
+        private void M_minus_Click(object sender, EventArgs e) //M- 버튼 처리 (M+ 와 동일)
         {
             if (memory != null)
             {
@@ -511,34 +507,34 @@ namespace Calculator
 /// 내부에서 사용하는 사용자 메서드 부분
 /// </summary>
 /// <param name="v"></param>
-        private void ShowHistory(string v)
+        private void ShowHistory(string v) //textbox2 위 텍스트박스(기록 모니터)에 데이터 띄우는 함수
         {
             textBox2.Text = v;
             textBox2.Refresh();
         }
-        private void ShowText(string v) //total 값 화면에 띄우기
+        private void ShowText(string v) //textbox1 아래 텍스트박스(값 모니터)에 데이터 띄우는 함수
         {
             textBox1.Text = v;
             textBox1.Refresh();
         }
 
-        private string ResultValue(double view)
+        private string ResultValue(double view) //연산식 처리 함수 현재 입력값을 매개변수로 받아옴
         {
             double result = 0;
-            input.Add(view.ToString());
+            input.Add(view.ToString()); //현재 입력값을 연산식 마지막에 등록
 
-            for (int i = 0; i < input.Count-1; ++i)
+            for (int i = 0; i < input.Count-1; ++i) //반복문을 이용한 연산
             {
-                if(i == 0)
+                if(i == 0) //첫 번째 숫자일때에 현재 값을 result에 등록
                 {
                     result = double.Parse(input[i]);
                 }
                 else
                 {
-                    switch(input[i])
+                    switch(input[i]) //switch로 연산기호에 따라 처리
                     {
                         case "+":
-                            result += double.Parse(input[i + 1]);
+                            result += double.Parse(input[i + 1]); //input[i] -> 현재 연산기호 input[i+1] -> 다음 값
                             break;
                         case "-":
                             result -= double.Parse(input[i + 1]);
@@ -547,10 +543,10 @@ namespace Calculator
                             result *= double.Parse(input[i + 1]);
                             break;
                         case "/":
-                            if (double.Parse(input[i + 1]) == 0)
+                            if (double.Parse(input[i + 1]) == 0) //0 나누기 에러 감지
                             {
 
-                                return "0 으로는 나눌 수 없습니다.";
+                                return "0 으로는 나눌 수 없습니다."; //연산 강제종료 하면서 에러메세지 리턴
                             }
                             else
                             {
@@ -561,29 +557,25 @@ namespace Calculator
                 }
             }
 
-            return result.ToString();
+            return result.ToString(); //최종 연산값 리턴
         }
 
-        private int DivisionError(string total)
+        private int DivisionError(string total) //0 나누기 에러 처리
         {
-            if(total.CompareTo("0 으로는 나눌 수 없습니다.") == 0)
+            if(total.CompareTo("0 으로는 나눌 수 없습니다.") == 0) //리턴받은 값이 에러메세지일때
             {
-                return 0;
+                return 0; //0 리턴
             }
-            else
+            else //정상 값이면 
             {
-                return 1;
+                return 1; //1 리턴
             }
         }
 
-        private void RebindingListBox(ListBox listbox, List<string> list)
+        private void RebindingListBox(ListBox listbox, List<string> list) //Listbox 업데이트 함수
         {
             listbox.DataSource = null;
             listbox.DataSource = list;
-        }
-
-
-
-       
+        } 
     }
 }
